7733bedc38e48ef41c6a0aeec5d78187e87f5aab
apetrovici <alejandropetrovici@gmail.com>
2025-01-11 16:52:02 +0100

    fix: type hint

--- app/backend/flask_app.py
+++ app/backend/flask_app.py
@@ -7,7 +7,7 @@ from app.backend.models.db import db
 from app.backend.routes.dashboard.dashboard import bp as bp_dashboard
 
 
-def create_app(config_class: Config) -> Flask:
+def create_app(config_class: type[Config] = Config) -> Flask:
     template_dir = Path(__file__).parent / "templates"
     static_dir = Path(__file__).parent / "static"
     app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
