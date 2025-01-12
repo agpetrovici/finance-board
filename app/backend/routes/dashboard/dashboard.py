5d8dd20af90e3c0e02de8c89d9ec171239716e11
apetrovici <alejandropetrovici@gmail.com>
2025-01-12 09:18:19 +0100

    chore: add link to dashboard.js

--- app/backend/routes/dashboard/dashboard.py
+++ app/backend/routes/dashboard/dashboard.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Blueprint, render_template
 
 # from app.backend.models.db import db
 
-bp = Blueprint("dashboard", __name__, static_folder="static", template_folder="templates", url_prefix="/")
+# Get the absolute path to the static folder
+current_dir = os.path.dirname(os.path.abspath(__file__))
+static_folder = os.path.join(current_dir, "static")
+
+bp = Blueprint("dashboard", __name__, static_folder=static_folder, static_url_path="/dashboard/static", template_folder="templates", url_prefix="/")
 
 
 @bp.route("/", methods=["GET"])
