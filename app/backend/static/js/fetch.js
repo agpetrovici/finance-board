4a5e68b4778947ffc295eacc6cbd96e4963326db
apetrovici <alejandropetrovici@gmail.com>
2025-01-14 21:59:50 +0100

    added: sendJSONgetJSON

--- app/backend/static/js/fetch.js
+++ app/backend/static/js/fetch.js
@@ -11,7 +11,27 @@ export async function getJSON(endpoint) {
 
   // Check if the response is OK (status 200-299)
   if (!response.ok) {
-    throw new Error(data.body || "Network response was not ok.");
+    throw new Error(data.message || "Network response was not ok.");
+  }
+
+  return data;
+}
+
+export async function sendJSONgetJSON(endpoint, object) {
+  // Send a POST request to the endpoint with the object as JSON
+  let response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(object),
+  });
+  // Read the response as json
+  let data = await response.json();
+
+  // Check if the response is OK (status 200-299)
+  if (!response.ok) {
+    throw new Error(data.message || "Network response was not ok.");
   }
 
   return data;
