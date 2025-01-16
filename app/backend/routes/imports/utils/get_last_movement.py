8ac6fe82a00f7acaceef1bac469077ebc2407cc0
apetrovici <alejandropetrovici@gmail.com>
2025-01-16 18:05:14 +0100

    refactor: update get_last_movement function to return Transaction object instead of bank_id

--- app/backend/routes/imports/utils/get_last_movement.py
+++ app/backend/routes/imports/utils/get_last_movement.py
@@ -4,9 +4,9 @@ from app.backend.models.db import db
 from app.backend.models.transaction import Transaction
 
 
-def get_last_movement(account_pk: int) -> Optional[str]:
+def get_last_movement(account_pk: int) -> Optional[Transaction]:
     last_movement = db.session.query(Transaction).filter(Transaction.account_fk == account_pk).order_by(Transaction.transaction_pk.desc()).first()
     if isinstance(last_movement, Transaction):
-        return str(last_movement.bank_id)
+        return last_movement
     else:
         return None
