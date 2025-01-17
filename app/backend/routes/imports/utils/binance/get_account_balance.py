89e8fe69596341d9140a2fab364bf1ea396fca72
apetrovici <alejandropetrovici@gmail.com>
2025-01-17 22:39:18 +0100

    added: initial get_account_balance

---
+++ app/backend/routes/imports/utils/binance/get_account_balance.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+
+from binance.client import Client
+
+from app.backend.models.db import db
+from app.backend.models.e_balance_crypto import BalanceCrypto
+
+
+def get_account_balance() -> None:
+    # Initialize the account
+    client = Client(os.getenv("BINANCE_API_KEY"), os.getenv("BINANCE_SECRET_KEY"))
+
+    # Fetch account snapshot (SPOT, MARGIN, FUTURES)
+    snapshot = client.get_account_snapshot(type="SPOT")
+
+    fk_account_crypto = 1
+
+    # Generate data
+    data = []
+    for element in snapshot["snapshotVos"]:
+        for day in element["data"]["balances"]:
+            unix_timestamp = element["updateTime"]
+            timestamp = datetime.fromtimestamp(unix_timestamp / 1000)
+            balance = BalanceCrypto(
+                fk_account_crypto=fk_account_crypto,
+                timestamp=timestamp,
+                asset=day["asset"],
+                free=day["free"],
+                locked=day["locked"],
+            )
+            data.append(balance)
+
+    # For each balance record, update if exists or insert if not
+    for balance in data:
+        existing = BalanceCrypto.query.filter_by(fk_account_crypto=balance.fk_account_crypto, timestamp=balance.timestamp, asset=balance.asset).first()
+
+        if existing:
+            existing.free = balance.free
+            existing.locked = balance.locked
+        else:
+            db.session.add(balance)
+
+    db.session.commit()
