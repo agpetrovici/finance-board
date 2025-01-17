a83464b9e118bf6557c60e9e6f711997157172d2
apetrovici <alejandropetrovici@gmail.com>
2025-01-17 22:37:32 +0100

    added: m_account_crypto

---
+++ app/backend/models/m_account_crypto.py
@@ -0,0 +1,15 @@
+from app.backend.models.db import db
+
+
+class AccountCrypto(db.Model):  # type: ignore[name-defined, misc]
+    __tablename__ = "account_crypto"
+
+    pk_account_crypto = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    exchange = db.Column(db.String(10))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self) -> str:
+        return f"<AccountCrypto {self.name}>"
