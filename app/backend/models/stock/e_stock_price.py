402794ed66ee74d81518f5fdc51420f21a565855
agpetrovici <alejandropetrovici@gmail.com>
2026-02-28 23:45:17 +0100

    update: StockPriceDaily model to use BigInteger for volume and remove nullable constraints on date and fk_symbol

--- app/backend/models/stock/e_stock_price.py
+++ app/backend/models/stock/e_stock_price.py
@@ -1,4 +1,6 @@
-from sqlalchemy import DateTime, ForeignKey, Integer, String, Float, PrimaryKeyConstraint
+import datetime
+
+from sqlalchemy import BigInteger, DateTime, ForeignKey, PrimaryKeyConstraint, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.backend.models.db import Base
@@ -8,14 +10,15 @@ class StockPriceDaily(Base):
     __tablename__ = "e_stock_price_daily"
     __table_args__ = (PrimaryKeyConstraint("fk_symbol", "date"),)
 
-    fk_symbol: Mapped[str] = mapped_column(String(10), ForeignKey("e_stock_symbol.pk_symbol"), nullable=False)
-    date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    fk_symbol: Mapped[str] = mapped_column(String(10), ForeignKey("e_stock_symbol.pk_symbol"))
+    date: Mapped[datetime.datetime] = mapped_column(DateTime)
 
-    open: Mapped[float] = mapped_column(Float, nullable=False)
-    high: Mapped[float] = mapped_column(Float, nullable=False)
-    low: Mapped[float] = mapped_column(Float, nullable=False)
-    close: Mapped[float] = mapped_column(Float, nullable=False)
-    volume: Mapped[int] = mapped_column(Integer, nullable=False)
+    open: Mapped[float]
+    high: Mapped[float]
+    low: Mapped[float]
+    close: Mapped[float]
+
+    volume: Mapped[int] = mapped_column(BigInteger)
 
     def __repr__(self) -> str:
         return f"<StockPriceDaily {self.fk_symbol} {self.date} {self.close}>"
