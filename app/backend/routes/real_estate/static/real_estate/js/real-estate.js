a774c0d706ba71c8188aeec58007b264790d42e8
agpetrovici <alejandropetrovici@gmail.com>
2026-02-28 15:26:31 +0100

    refactor: streamline comparables handling in map by implementing a lookup function and enhancing popup content

--- app/backend/routes/real_estate/static/real_estate/js/real-estate.js
+++ app/backend/routes/real_estate/static/real_estate/js/real-estate.js
@@ -1,14 +1,3 @@
-var comparableIcon = L.icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
 async function initMap() {
   var map = L.map("map").setView([0, 0], 3);
 
@@ -56,70 +45,66 @@ async function initMap() {
     hasMarkers = true;
   });
 
-  var comparablesLayer = L.layerGroup();
-  comparables.forEach(function (c) {
-    //   Content to display in the popup
-    var popupContent =
-      "<strong>" +
-      c.reference +
-      "</strong><br>" +
-      "<b>Surface:</b> " +
-      c.surface.toLocaleString() +
-      " m²<br>" +
-      (c.avg_price
-        ? "<b>Avg Price:</b> " + c.avg_price.toLocaleString() + " €<br>"
-        : "") +
-      (c.avg_price_per_m2
-        ? "<b>Avg €/m²:</b> " + c.avg_price_per_m2.toLocaleString() + "<br>"
-        : "") +
-      (c.listings_count
-        ? "<b>Listings:</b> " + c.listings_count + "<br>"
-        : "") +
-      "<b>Address:</b> " +
-      c.address +
-      "<br>" +
-      '<a href="https://www.google.com/maps?q=' +
-      c.lat +
-      "," +
-      c.lng +
-      '" target="_blank" rel="noopener">Open in Google Maps</a>' +
-      (c.listings && c.listings.length
-        ? "<br>" +
-          c.listings
-            .map(function (l) {
-              return (
-                '<a href="' +
-                l.url +
-                '" target="_blank" rel="noopener">' +
-                l.title +
-                "</a>"
-              );
-            })
-            .join("<br>")
-        : "");
-
-    L.marker([c.lat, c.lng], { icon: comparableIcon })
-      .bindPopup(popupContent)
-      .addTo(comparablesLayer);
-    bounds.extend([c.lat, c.lng]);
-    hasMarkers = true;
-  });
+  var comparableLookup = buildComparableLookup(comparables);
 
   var parcelsLayer = L.geoJSON(null, {
-      style: { color: "#e67e22", weight: 2, fillOpacity: 0.15 },
-      onEachFeature: function (feature, layer) {
-          layer.bindPopup("<b>Ref:</b> " + feature.properties.refcat);
-      },
+    style: { color: "#e67e22", weight: 2, fillOpacity: 0.15 },
+    onEachFeature: function (feature, layer) {
+      var refcat = feature.properties.refcat;
+      var items = comparableLookup[refcat] || [];
+      var popupContent = "<strong>" + refcat + "</strong>";
+
+      items.forEach(function (c) {
+        popupContent +=
+          "<hr style='margin:4px 0'>" +
+          "<b>Surface:</b> " +
+          c.surface.toLocaleString() +
+          " m²<br>" +
+          (c.avg_price
+            ? "<b>Avg Price:</b> " + c.avg_price.toLocaleString() + " €<br>"
+            : "") +
+          (c.avg_price_per_m2
+            ? "<b>Avg €/m²:</b> " + c.avg_price_per_m2.toLocaleString() + "<br>"
+            : "") +
+          (c.listings_count
+            ? "<b>Listings:</b> " + c.listings_count + "<br>"
+            : "") +
+          "<b>Address:</b> " +
+          c.address +
+          "<br>" +
+          '<a href="https://www.google.com/maps?q=' +
+          c.lat +
+          "," +
+          c.lng +
+          '" target="_blank" rel="noopener">Google Maps</a>' +
+          (c.listings && c.listings.length
+            ? "<br>" +
+              c.listings
+                .map(function (l) {
+                  return (
+                    '<a href="' +
+                    l.url +
+                    '" target="_blank" rel="noopener">' +
+                    l.title +
+                    "</a>"
+                  );
+                })
+                .join("<br>")
+            : "");
+      });
+
+      layer.bindPopup(popupContent, { maxHeight: 300 });
+      bounds.extend(layer.getBounds());
+      hasMarkers = true;
+    },
   });
 
   propertiesLayer.addTo(map);
-  comparablesLayer.addTo(map);
   parcelsLayer.addTo(map);
 
   L.control
     .layers(null, {
-      "Properties": propertiesLayer,
-      "Comparables": comparablesLayer,
+      Properties: propertiesLayer,
       "Cadastral Parcels": parcelsLayer,
     })
     .addTo(map);
@@ -132,14 +117,24 @@ async function initMap() {
   loadCadastralParcels(parcelsLayer);
 }
 
+function buildComparableLookup(comparables) {
+  var lookup = {};
+  comparables.forEach(function (c) {
+    var refcat14 = c.reference.substring(0, 14);
+    if (!lookup[refcat14]) lookup[refcat14] = [];
+    lookup[refcat14].push(c);
+  });
+  return lookup;
+}
+
 async function loadComparablesTable() {
-    var response = await fetch("/real-estate/api/comparables-table");
-    var html = await response.text();
-    document.getElementById("comparables-table").innerHTML = html;
+  var response = await fetch("/real-estate/api/comparables-table");
+  var html = await response.text();
+  document.getElementById("comparables-table").innerHTML = html;
 }
 
 async function loadCadastralParcels(parcelsLayer) {
-    var response = await fetch("/real-estate/api/cadastral-parcels");
-    var geojson = await response.json();
-    parcelsLayer.addData(geojson);
+  var response = await fetch("/real-estate/api/cadastral-parcels");
+  var geojson = await response.json();
+  parcelsLayer.addData(geojson);
 }
